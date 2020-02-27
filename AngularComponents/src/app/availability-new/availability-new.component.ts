6a152bab539b24449abe7822a382ff6005be92c4
Marc Martínez-Carrasco Compaño <marc.martinez-carrasco@pcbcn11.koedia.lan>
2020-02-27 17:31:28 +0100

    step logic for creating new avails

--- AngularComponents/src/app/availability-new/availability-new.component.ts
+++ AngularComponents/src/app/availability-new/availability-new.component.ts
@@ -9,6 +9,9 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 import { ModalService } from '../services/modal.service';
 import { FormControl, FormGroup, FormBuilder ,FormArray,Validators } from '@angular/forms';
 import { Months } from './months-enum';
+import { RoomType } from 'src/DTO/availabilityNew/roomType';
+import { ClientProgram } from 'src/DTO/availabilityNew/clientProgram';
+import { analyzeFileForInjectables } from '@angular/compiler';
 
 @Component({
   selector: 'availability-new',
@@ -21,25 +24,45 @@ export class AvailabilityNewComponent implements OnInit {
   availAdd:AvailabilityNewGet;
   test:any;
   opened:boolean = false;
+  //booleans for step display control
   showPeriods:boolean = false;
   showAvailType:boolean = false;
   showCreateAvail:boolean = false;
+  //Form management
   periodsFrom:FormArray =  new FormArray([]);
   periodsTo:FormArray =  new FormArray([]);
-
+  roomType:Array<any> = new Array();
+  distributionType:Array<ClientProgram> = new Array();
+  activateDistributionSelect:boolean = false;
+  //Form step 1 periods
   periodsForm = this.fb.group({
     // nom : ['', Validators.required],
     // cognoms: ['', Validators.compose( [Validators.minLength(5),Validators.required])],
     periodsFrom: this.periodsFrom,
     periodsTo: this.periodsTo
   });
-
-  createAvailForm = this.fb.group({});
-
+  //Form step 3 create avail
+  createAvailForm = this.fb.group({
+    roomType: ['',Validators.required],
+    distributionType: new FormControl({value:'',disabled: this.activateDistributionSelect},Validators.required),
+    weekDaysMo:new FormControl(''),
+    weekDaysTu:new FormControl(''),
+    weekDaysWe:new FormControl(''),
+    weekDaysTh:new FormControl(''),
+    weekDaysFr:new FormControl(''),
+    weekDaysSa:new FormControl(''),
+    weekDaysSu:new FormControl(''),
+    all:new FormControl(''),
+    dayFrom:new FormControl(''),
+    dayTo:new FormControl(''),
+    stayMin:new FormControl(''),
+    stayMax:new FormControl(''),
+  });
+  //Form step 2 avail type
   availTypeForm = this.fb.group({
     availType: ['',Validators.required]
   });
-
+  //constructor
   constructor(private availService:AvailabilityService,public dialog: MatDialog,private modalService: ModalService,
     private el: ElementRef, private fb: FormBuilder) { }
   
@@ -48,13 +71,14 @@ export class AvailabilityNewComponent implements OnInit {
     this.availService.getAvailabilitiesByAccom()).subscribe(([res1, res2]) => {
       console.log(res1);
       this.availAdd = res2;
+      for(let key of this.availAdd.mapRoomTypeDTOClientProgram.entries()){
+        this.roomType.push(key[1][0]);
+      }
     });
 
     this.periodsFrom.push(this.fb.control('',Validators.required));
     this.periodsTo.push(this.fb.control('',Validators.required));
-    // console.debug(Months[2]);
-    // let month:any = Months[2];
-    
+
   }
 
   ngOnLoad(){
@@ -79,24 +103,25 @@ export class AvailabilityNewComponent implements OnInit {
   }
 
   onSubmit() {
-    let periodsFromTo = new Array<PeriodFromTo>();
+    let periodsFromToDTO = new Array<PeriodFromTo>();
     for (let index = 0; index < this.periodsFrom.length; index++) {
-      let periodFromtoDTO = new PeriodFromTo(this.periodsFrom[index].value,this.periodsTo[index].value);
-      periodsFromTo.push(periodFromtoDTO);
+      let periodFromtoDTO = new PeriodFromTo(this.periodsFrom.at(index).value,this.periodsTo.at(index).value);
+      periodsFromToDTO.push(periodFromtoDTO);
     }
-    // let availPost:AvailabilityAddPostDTO = new AvailabilityAddPostDTO(this.availAddForm.controls.nom.value,
-    //   periodsFromTo);
-    console.debug(periodsFromTo);
+    console.debug(periodsFromToDTO);
   }
 
   showDialog(){
     this.opened = true;
     this.showPeriods = true;
+    this.showAvailType = false;
+    this.showCreateAvail = false;
   }
 
   closeDialog() {
     this.opened = false;
   }
+  
 
   changeModalStep(step:string){
     console.warn("inside changeModalStep")
@@ -105,17 +130,42 @@ export class AvailabilityNewComponent implements OnInit {
         this.showPeriods = true;
         this.showAvailType = false;
         this.showCreateAvail = false;
-        console.warn('showPeriods')
+        //console.warn('showPeriods')
     }else if(step == "showAvailType"){
         this.showPeriods = false;
         this.showAvailType = true;
         this.showCreateAvail = false;
-        console.warn('showAvailType')
+        //console.warn('showAvailType')
     }else if(step == "showCreateAvail"){
         this.showPeriods = false;
         this.showAvailType = false;
         this.showCreateAvail = true;
-        console.warn('showCreateAvail')
+        //console.warn('showCreateAvail')
+    }
+  }
+
+  activatedistributionTypeSelect(){
+    //console.warn("this.createAvailForm.controls['roomType'].value: "+this.createAvailForm.controls['roomType'].value)
+    this.distributionType = new Array<ClientProgram>();
+    
+    if(this.createAvailForm.controls['roomType'].value != ""){
+      for(let entry of this.availAdd.mapRoomTypeDTOClientProgram.entries()){
+        let mappingroomProgramIds:any = entry[1][0];
+        //check the roomId selected in the mapping between rooms and programs       
+        if(mappingroomProgramIds.id == this.createAvailForm.controls['roomType'].value){;
+          let programIdList:any = entry[1][1];
+          for(let id of programIdList){
+            for(let clientProgram of this.availAdd.inventoryOfPrograms){
+              if(id == clientProgram.clientProgramId){
+                this.distributionType.push(clientProgram);
+              }
+            }
+          }
+        }
+      }
+      this.activateDistributionSelect = true;
+    }else{
+      this.activateDistributionSelect = false;
     }
   }
 }
